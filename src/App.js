d71143688cf08bd7046ffce8c41b762203d8c58c
Juan Manuel Diaz <juanmanueldiaz85@gmail.com>
2020-09-01 23:22:13 -0300

    Fix PNF component missing

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-d
 import PokemonState from "./context/pokedex/PokedexState";
 import PokedexMain from "./components/PokedexMain";
 import PokemonDetails from "./components/PokemonDetails";
-import PageNotFound from "./components/PageNotFound";
 
 const App = () => {
   return (
@@ -13,9 +12,6 @@ const App = () => {
           <Route exact path="/pokedex" component={PokedexMain} />
           <Route exact path="/pokemon/:name" component={PokemonDetails} />
           <Redirect from="/" to="/pokedex"/>
-          <Route path="*">
-            <PageNotFound />
-          </Route>
         </Switch>
       </Router>
     </PokemonState>
